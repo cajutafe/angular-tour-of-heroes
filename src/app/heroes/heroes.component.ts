70c3c529b76c9cb92a62bb1b00fe52f2204be593
Julian Tarazona <tarazonacarlosjulian@gmail.com>
2022-09-18 20:03:08 +0200

    Servicio inyectable+@inject asincrono+ mensajeserv

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes'; Ya no es necesaria porque usaremos el servicio HEROSERVICE
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +13,9 @@ import { HEROES } from '../mock-heroes';
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  // heroes = HEROES; DEFINICIón para el import sin servicio
+  heroes: Hero[] = [];
+
   // Opciones para corregir el error de inicializador no definido correctamente en el constructos:
   // selectedHero?: Hero;
   // selectedHero!: Hero;
@@ -23,12 +27,30 @@ export class HeroesComponent implements OnInit {
   //   name: 'Windstorm' 
   // };
 
-  constructor() { }
+  constructor(
+    //Usamos el contructor para inicializaciones simplres.
+    private heroService: HeroService,
+    //Añadimos el mensaje de cada vez que se clique en un heroe
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
+    //LLamamos getHeroes a través de ngOnInit para una mejor práctica
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    //Mensaje de cada Heroe seleccionado
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void{
+    //Llamada sincrona, solo para local lo que si llamaramos de manera remota a un servidor nos tardaría en cargar la página además de no perminitrnos hacer nada en ella.
+    // this.heroes = this.heroService.getHeroes();
+
+    //LLAMADA ASINCRONA
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
